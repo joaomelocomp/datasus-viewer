31427f93296bb8135de4af96c1469bf41d071e8f
joaoprogram <joaogabriel.s.melo92@gmail.com>
2026-08-24 21:03:49 -0300

    fix: configure flask for render

--- app.py
+++ app.py
@@ -175,5 +175,10 @@ def api_chart():
     return jsonify(figure=fig_json)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
